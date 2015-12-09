0f213e12cc0e938e8ecded527f6fc27ee8a993a1
Mateus Freira <mateus.freira@gmail.com>
2015-12-09 21:51:29 -0200

    Fixing #7

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,30 @@
 
+// FILE: scripts/docbase-html-mode.js
+/**
+ *
+ * Docbase engine
+ * Appbase
+ * MIT license
+ *
+ */
+
+(function($, angular) {
+    /*
+     * Scrollspy.
+     */
+
+    $(function() {
+        $("h2, h3").scrollagent(function(cid, pid, currentElement, previousElement) {
+            if (pid) {
+                $("[href='#" + pid + "']").removeClass('active');
+            }
+            if (cid) {
+                $("[href='#" + cid + "']").addClass('active');
+            }
+        });
+    });
+})(window.jQuery, window.angular);
+
 // FILE: scripts/docbase.js
 /**
  *
---
+++ scripts/docbase-html-mode.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * Docbase engine
+ * Appbase
+ * MIT license
+ *
+ */
+
+(function($, angular) {
+    /*
+     * Scrollspy.
+     */
+    $(function() {
+        $("h2, h3").scrollagent(function(cid, pid, currentElement, previousElement) {
+            if (pid) {
+                $("[href='#" + pid + "']").removeClass('active');
+            }
+            if (cid) {
+                $("[href='#" + cid + "']").addClass('active');
+            }
+        });
+    });
+})(window.jQuery, window.angular);
