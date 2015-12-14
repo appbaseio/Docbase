f658ca6ca8f7863bdf156fad96191c44b7a9ac39
Farhan Chauhan <farhan687@gmail.com>
2015-12-14 11:00:21 +0530

    Add highlight feature - highlight the matching words in current document while searching.

--- scripts/searchAppbase.js
+++ scripts/searchAppbase.js
@@ -49,6 +49,10 @@
 			$search.bind('typeahead:close', function(ev, suggestion) {
 				$search.parents('.search-form').removeClass('open');
 			});
+			$search.on('keyup', function() {
+				var searchText = $(this).val();
+				$('.content').removeHighlight().highlight(searchText);
+			});
 		};
 
 		//Fetch search index json data
@@ -113,5 +117,46 @@
 				intializeCall();
 			});
 
+	};
+}(jQuery));
+
+(function($) {
+	$.fn.highlight = function(pat) {
+		function innerHighlight(node, pat) {
+			var skip = 0;
+			if (node.nodeType == 3) {
+				var pos = node.data.toUpperCase().indexOf(pat);
+				if (pos >= 0) {
+					var spannode = document.createElement('span');
+					spannode.className = 'highlight';
+					var middlebit = node.splitText(pos);
+					var endbit = middlebit.splitText(pat.length);
+					var middleclone = middlebit.cloneNode(true);
+					spannode.appendChild(middleclone);
+					middlebit.parentNode.replaceChild(spannode, middlebit);
+					skip = 1;
+				}
+			} else if (node.nodeType == 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
+				for (var i = 0; i < node.childNodes.length; ++i) {
+					i += innerHighlight(node.childNodes[i], pat);
+				}
+			}
+			return skip;
+		}
+		return this.length && pat && pat.length ? this.each(function() {
+			innerHighlight(this, pat.toUpperCase());
+		}) : this;
+	};
+}(jQuery));
+
+(function($) {
+	$.fn.removeHighlight = function() {
+		return this.find("span.highlight").each(function() {
+			this.parentNode.firstChild.nodeName;
+			with(this.parentNode) {
+				replaceChild(this.firstChild, this);
+				normalize();
+			}
+		}).end();
 	};
 }(jQuery));
