9bad19275364fc8623437efda294c6eecf8bddf9
farhan <farhan687@gmail.com>
2016-09-21 11:02:37 +0530

    Load jquery with typeahead

--- scripts/searchAppbase.js
+++ scripts/searchAppbase.js
@@ -146,6 +146,7 @@
 
 		var jquery_js = new Loader();
 		jquery_js.require([
+				"https://code.jquery.com/jquery-2.2.4.min.js",
 				"https://twitter.github.io/typeahead.js/releases/latest/typeahead.bundle.js"
 			],
 			function() {
