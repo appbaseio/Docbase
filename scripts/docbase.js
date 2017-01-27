b8daecda9d3e36789ccb83c8a94d580cd82db7d1
farhan687 <farhan687@gmail.com>
2017-01-27 12:12:51 +0530

    Add new feature - navigation sidebar

--- scripts/docbase.js
+++ scripts/docbase.js
@@ -410,12 +410,14 @@
             } catch (e) {
               markdown.unshift(obj);
               obj = {
-                'threeColumns': false
+                'threeColumns': false,
+                'navigationSidebar': true
               };
             }
 
             markdown = markdown.join('\n');
-
+            retObj.threeColumns = obj.threeColumns;
+            retObj.navigationSidebar = obj.navigationSidebar;
             if (obj.threeColumns) {
               $('body').removeClass('no-literate');
             } else {
@@ -483,7 +485,13 @@
       $scope.navbarHtml = Docbase.options.navbarHtml;
       $scope.logoSrc = Docbase.options.logoSrc;
       $scope.docbaseOptions = Docbase.options;
-
+      $scope.threeColumns = data.threeColumns;
+      $scope.navigationSidebar = data.navigationSidebar;
+      var urlSplit = data.locationPath.split('/');
+      var tempFolder = urlSplit[urlSplit.length-2];
+      var tempFile = urlSplit[urlSplit.length-1];
+      $scope.currentFolder = data.currentFolder ? data.currentFolder : tempFolder;
+      $scope.currentFile = data.currentFile ? data.currentFile : tempFile;
       setTimeout(function(){
         $('#folder-navbar').megaMenu();
       },200);
@@ -496,7 +504,6 @@
       //If index is true
       if (data.index) {
         $scope.index = true;
-        $scope.currentFolder = data.currentFolder;
         $scope.indexList = [];
 
         for (var version in data.map) {
